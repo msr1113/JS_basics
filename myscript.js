ae113fdc99eaa0cf64e0cc74091899c73db549fe
mrawal <imsr1013@gmail.com>
2022-07-08 12:06:54 +0545

    if else temperature check

--- myscript.js
+++ myscript.js
@@ -1,4 +1,16 @@
-var lbs = prompt("Weight in lbs");
-var kg = lbs*0.454;
-alert("That is: "+kg+" kilograms");
-console.log("Conversion complete")
+var hot = false
+var temp = 70
+
+if(temp>80){
+    console.log("Hot outside")
+}
+else if (temp<=80 && temp >=50) {
+    console.log("avg temp outside")
+}
+else if(temp<50 && temp >=32){
+    console.log("it pretty cold out")
+}
+else {
+    console.log("its not very cold today!")
+}
+
