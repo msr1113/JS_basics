431e03896a808cc56c7652ee059ba42e140b5219
mrawal <imsr1013@gmail.com>
2022-07-08 12:11:41 +0545

    if else check 2

--- myscript.js
+++ myscript.js
@@ -1,16 +1,14 @@
-var hot = false
-var temp = 70
+var ham = 0
+var cheese = 0
 
-if(temp>80){
-    console.log("Hot outside")
-}
-else if (temp<=80 && temp >=50) {
-    console.log("avg temp outside")
-}
-else if(temp<50 && temp >=32){
-    console.log("it pretty cold out")
-}
-else {
-    console.log("its not very cold today!")
+var report = "blank";
+
+if (ham >= 10 && cheese >= 10) {
+    report = "Strone sales of both ham and cheese!"
+} else if(ham === 0 && cheese === 0) {
+    report = "Nothing sold!"
+} else {
+    report = "We had some sales of items"
 }
 
+console.log(report)
