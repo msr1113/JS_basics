67a4098a8bf42113eae3b26e879b5c8c75fa5369
mrawal <imsr1013@gmail.com>
2022-07-08 14:37:08 +0545

    Prompt and check condition using prompt values

--- myscript.js
+++ myscript.js
@@ -1,17 +1,54 @@
-// use loops to console.log() all odd numbers from 1 to 25
-// do this using two methods, while loop and for loop
+var firstName = prompt("first name please:")
+var lastName = prompt("last name please")
+var age = prompt("how old are you")
+var height = prompt("what is your height")
+var petName = prompt("what is your pet name?")
+alert("Thank you so much for the information")
 
-// using for loop
 
-// for(x=1;x<26;x=x+2){
-//     console.log(x)
-// }
+var nameCond = null
+var ageCond = null
+var heightCond = null
+var petCond = null
 
-// using while loop
-
-x=1
-
-while(x<26){
-    console.log('using while:',x)
-    x=x+2
+//named condition
+if (firstName[0]  === lastName[0]) {
+    nameCond = this;
 }
+else {
+    nameCond = false
+}
+
+//age condition
+
+if(age>20 && age<30){
+    ageCond = true
+}else{
+    ageCond = false
+}
+
+// height condition
+
+if(height>=170){
+    heightCond = true
+}else{
+    heightCond = false
+}
+
+//penCond
+if (petName[petName.length-1] === 'y' ){
+    petCond = true
+}
+else{
+    petCond = false
+}
+
+// check all conditions
+
+if (nameCond && ageCond && heightCond && petCond) {
+    console.log('welcome spy')
+}
+else{
+    console.log('nothing to see here')
+}
+
