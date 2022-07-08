7b3ef2736088c6844fe79657fe31370b88d8ad28
mrawal <imsr1013@gmail.com>
2022-07-08 17:06:52 +0545

    global and local scope in javascript

---
+++ functions.js
@@ -0,0 +1,25 @@
+// function helloSomeone(name="Frankie"){
+//     console.log("Hello "+name)
+// }
+
+// function formal(name="Sam",title='Sir'){
+//     return(title+" "+name)
+// }
+
+function timesFive(numInput) {
+    var result = numInput * 5
+    return result
+}
+
+//global scope
+var v = "GLOBAL V"
+var stuff = "GLOBAL STUFF"
+
+function fun(stuff) {
+    console.log(v);
+    stuff = "Reassign stuff inside func"
+    console.log(stuff)
+}
+
+fun()
+console.log(stuff)
