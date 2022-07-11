ae53e00d817f66191a56dd8b100d9f80913d60d2
mrawal <imsr1013@gmail.com>
2022-07-11 10:07:31 +0545

    function to return string 3 times

--- functions.js
+++ functions.js
@@ -6,20 +6,44 @@
 //     return(title+" "+name)
 // }
 
-function timesFive(numInput) {
-    var result = numInput * 5
-    return result
+// function timesFive(numInput) {
+//     var result = numInput * 5
+//     return result
+// }
+//
+// //global scope
+// var v = "GLOBAL V"
+// var stuff = "GLOBAL STUFF"
+//
+// function fun(stuff) {
+//     console.log(v);
+//     stuff = "Reassign stuff inside func"
+//     console.log(stuff)
+// }
+//
+// fun()
+// console.log(stuff)
+
+// function sleepIn(weekday,vacation) {
+//     return (!weekday || vacation)
+// }
+
+
+// function monkeyTrouble(aSmile,bSmile) {
+//     return (aSmile && bSmile)  || (!aSmile && !bSmile)
+// }
+
+function stringTimes(str,n) {
+    console.log('function called')
+    var returnStr = "";
+    var i = 0;
+    while (i<n){
+        returnStr=returnStr+str
+
+        i=i+1
+    }
+    return returnStr
 }
 
-//global scope
-var v = "GLOBAL V"
-var stuff = "GLOBAL STUFF"
-
-function fun(stuff) {
-    console.log(v);
-    stuff = "Reassign stuff inside func"
-    console.log(stuff)
-}
-
-fun()
-console.log(stuff)
+a = stringTimes("hi",3)
+console.log(a)
