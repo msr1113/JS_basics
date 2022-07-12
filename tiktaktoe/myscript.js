2e6060445d7f1a91f943f1396c1724b378d8eaa9
mrawal <imsr1013@gmail.com>
2022-07-12 16:08:56 +0545

    Tik Tak Toe solution 2 using function changeMarker while using this keyword and connecting squares with addEventlistener inside for loop

--- tiktaktoe/myscript.js
+++ tiktaktoe/myscript.js
@@ -15,15 +15,18 @@ restart.addEventListener('click',clearBoard)
 
 //check the square marker
 
-var cellOne = document.querySelector('#one')
-cellOne.addEventListener('click',function() {
-    if(cellOne.textContent === ''){
-        cellOne.textContent ='X'
-    }else if(cellOne.textContent === 'X'){
-        cellOne.textContent ='O'
-    }else{
-        cellOne.textContent = ''
+function changeMarker()
+{
+    if(this.textContent === ''){
+        this.textContent = 'X';
+    }else if(this.textContent === 'X'){
+        this.textContent = 'O'
+    }else {
+        this.textContent =''
     }
-})
-
+}
 // for loop to add event listeners to all the squares
+
+for(var i=0;i<squares.length;i++){
+    squares[i].addEventListener('click',changeMarker)
+}
